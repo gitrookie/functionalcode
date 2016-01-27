39b66ff09b63336ced754a6e25a18578ee32bd3f
gitrookie <gaurav3184@gmail.com>
2016-01-27 20:15:10 +0530

    [Haskell Monads]

---
+++ code/Haskell/snippets/hmonads.hs
@@ -0,0 +1,8 @@
+-- hmonads.hs
+import qualified Control.Monad
+import System.Environment (getArgs)
+main :: IO ()
+-- main = getLine >>= putStrLn
+-- main = putStrLn =<< getLine
+-- main = putStrLn  Control.Monad.>=> putStrLn
+           
